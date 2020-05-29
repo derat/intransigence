443bdca844fbecba82e05c8180eef5be153974c3
Daniel Erat <dan@erat.org>
2020-05-29 14:56:35 -0400

    render: Update gen_filemap.go to include hash in generated file.

--- render/gen_test.go
+++ render/gen_test.go
@@ -4,45 +4,73 @@
 package render
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
 	"fmt"
-	"os"
+	"io/ioutil"
 	"path/filepath"
+	"regexp"
+	"sort"
 	"testing"
 )
 
-// checkGenerated returns an error if file's modification time precedes that of any top-level file in dir.
-func checkGenerated(file, dir string) error {
-	fi, err := os.Stat(file)
-	if err != nil {
-		return err
-	}
+var hashRegexp = regexp.MustCompile(` from (.+)\. DO NOT EDIT\.`)
 
-	d, err := os.Open(dir)
+// checkGenerated returns an error if the hash in file doesn't match the globbed files.
+// This needs to match the hashing logic in gen/gen_filemap.go.
+func checkGenerated(file string, globs []string) error {
+	b, err := ioutil.ReadFile(file)
 	if err != nil {
 		return err
 	}
-	defer d.Close()
+	ms := hashRegexp.FindSubmatch(b)
+	if len(ms) != 2 {
+		return errors.New("failed to find hash in generated file")
+	}
+	fileHash := string(ms[1])
 
-	dfis, err := d.Readdir(0)
-	if err != nil {
-		return err
-	}
-	for _, dfi := range dfis {
-		if fi.ModTime().Before(dfi.ModTime()) {
-			return fmt.Errorf("%v is older than %v", file, filepath.Join(dir, dfi.Name()))
+	var paths []string
+	for _, glob := range globs {
+		ps, err := filepath.Glob(glob)
+		if err != nil {
+			return err
+		} else if len(ps) == 0 {
+			return fmt.Errorf("no files matched by %q", glob)
 		}
+		paths = append(paths, ps...)
+	}
+
+	hash := sha256.New()
+	sort.Strings(paths)
+	for _, p := range paths {
+		b, err := ioutil.ReadFile(p)
+		if err != nil {
+			return err
+		}
+		hash.Write([]byte(filepath.Base(p)))
+		hash.Write([]byte{0})
+		hash.Write([]byte(base64.StdEncoding.EncodeToString(b)))
+		hash.Write([]byte{0})
+	}
+	globHash := hex.EncodeToString(hash.Sum(nil))
+
+	if fileHash != globHash {
+		return fmt.Errorf("hash in %v (%v) doesn't match source hash (%v)", file, fileHash, globHash)
 	}
 	return nil
 }
 
 func TestStdInline(t *testing.T) {
-	if err := checkGenerated("std_inline.go", "inline"); err != nil {
-		t.Error("Checking generated inline files failed: ", err)
+	// TODO: Figure out some way to check that gen_css.sh and gen_js_min.sh have been run.
+	if err := checkGenerated("std_inline.go", []string{"inline/*.css", "inline/*.js.min"}); err != nil {
+		t.Error("Checking generated inline files failed:", err)
 	}
 }
 
 func TestStdTemplates(t *testing.T) {
-	if err := checkGenerated("std_templates.go", "templates"); err != nil {
-		t.Error("Checking generated templates failed: ", err)
+	if err := checkGenerated("std_templates.go", []string{"templates/*.tmpl"}); err != nil {
+		t.Error("Checking generated templates failed:", err)
 	}
 }
