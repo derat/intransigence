5bce0f841948b8879c5d8237348cc06dbec35a43
Daniel Erat <dan@erat.org>
2022-06-23 09:34:47 -0400

    render: Move logo outside of <nav> for non-AMP.
    Also try to clean up usage of header vs. nav in CSS.

--- render/inline/base.js
+++ render/inline/base.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('nav ul').classList.toggle('collapsed-mobile');
     document.querySelector('nav .toggle').classList.toggle('expand');
   };
-  document.querySelector('nav .logo').addEventListener('click', toggle);
+  document.querySelector('header .logo').addEventListener('click', toggle);
   document.querySelector('nav .box .title').addEventListener('click', toggle);
 });
