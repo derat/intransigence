derat/intransigence

commit 9eefadce87c9338dab9f346536ea07c57f3d7738

render: Hack to fix map placeholder aspect ratio on AMP.

render/inline/amp.css
@@ -1,1 +1,1 @@
-amp-img.thumb{filter:blur(12px)}
+amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{width:auto !important}

render/inline/amp.scss
@@ -3,3 +3,8 @@
 amp-img.thumb {
   filter: blur(12px);
 }
+main .box > .body .mapbox amp-img[placeholder] {
+  // I hate this. AMP seems to programatically set amp-iframe placeholders'
+  // widths to 640px, which results in the images being stretched horizontally.
+  width: auto !important;
+}

render/std_inline.go
@@ -1,11 +1,11 @@
-// Code generated by gen_filemap.go from 2a3ef9516b75d045c9b5287a9715efb67919f8b59ad556a0e85b0fc7431dd798. DO NOT EDIT.
+// Code generated by gen_filemap.go from f5036a4585956c96b8e846b1d4e519b2e8cd77790ed29dd64a1162798a8e8aaf. DO NOT EDIT.
 
 package render
 
 var stdInline = map[string]string{
 	"amp-boilerplate-noscript.css": "body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}",
 	"amp-boilerplate.css":          "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}",
-	"amp.css":                      "amp-img.thumb{filter:blur(12px)}\n",
+	"amp.css":                      "amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{width:auto !important}\n",
 	"base-body.js":                 "applyTheme(); // defined in dark.js\n",
 	"base.css":                     "header .dark{cursor:pointer}main .box{display:block}main .box>.body:after{clear:both;content:'';display:block}main .box>.body>*:first-child,main .box>.body>*:first-child>h3:first-child{margin-top:0}main .box>.body>*:last-child{margin-bottom:0}main .box>.body figure.left{float:left}main .box>.body figure.right{float:right}main .box>.body figure.center{margin-left:auto;margin-right:auto}main .box>.body figure *{max-width:100%}main .box>.body figure img{border:0;display:block;height:auto}main .box>.body img.inline,main .box>.body amp-img.inline{vertical-align:middle}main .box>.body img.pixelated,main .box>.body amp-img.pixelated{image-rendering:pixelated}main .box>.body img.inline{display:inline}main .box>.body pre{max-width:100%;white-space:pre-wrap;word-wrap:break-word}main .box>.body table{border-collapse:collapse}main .box>.body .clear{clear:both}main .box>.body .small{font-size:90%}main .box>.body .real-small{font-size:80%}main .box>.body .no-select{user-select:none}\n",
 	"base.js":                      "document.addEventListener('DOMContentLoaded', () => {\n  // Toggle the navbox when the logo or anything in its title are clicked.\n  const toggle = () => {\n    document.querySelector('nav ul').classList.toggle('collapsed-mobile');\n    document.querySelector('nav .toggle').classList.toggle('expand');\n  };\n  document.querySelector('header .logo').addEventListener('click', toggle);\n  document.querySelector('nav .box .title').addEventListener('click', toggle);\n\n  // |darkQuery| and applyTheme() are defined in dark.js.\n  // Toggle the theme when the dark-mode icon is clicked.\n  // The initial state is set in base-body.js: we can't do this in the top level\n  // of this file since document.body isn't available, and we also don't want to\n  // do it in DOMContentLoaded since we'll get a flash of the light theme then.\n  document\n    .querySelector('header .dark')\n    .addEventListener('click', () => applyTheme(true));\n\n  // We may also need to update the theme if prefers-color-scheme changes.\n  darkQuery.addEventListener('change', () => applyTheme());\n});\n",

render/page.go
@@ -597,7 +597,6 @@ func (r *renderer) renderCodeBlock(w io.Writer, node *bf.Node, entering bool) bf
 			r.setErrorf("failed to parse map info from %q: %v", node.Literal, err)
 			return bf.Terminate
 		}
-		info.imgInfo.layout = "fill"
 		info.imgInfo.Attr = append(info.imgInfo.Attr, template.HTMLAttr("placeholder"))
 		info.imgInfo.Alt = "[map placeholder]"
 		info.imgInfo.noThumb = true // already a placeholder

commit 314af8e48208c1fba61b7ba69fcde12e3f78aa07

render: Fix AMP map placeholder aspect ratio using max-width.
9eefadce87c9 broke validation by using '!important', which
is disallowed by AMP.

render/inline/amp.css
@@ -1,1 +1,1 @@
-amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{width:auto !important}
+amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{max-width:100%}

render/inline/amp.scss
@@ -4,7 +4,8 @@ amp-img.thumb {
   filter: blur(12px);
 }
 main .box > .body .mapbox amp-img[placeholder] {
-  // I hate this. AMP seems to programatically set amp-iframe placeholders'
-  // widths to 640px, which results in the images being stretched horizontally.
-  width: auto !important;
+  // AMP appears to programatically set placeholders' widths to 640px, so
+  // constrain them to avoid stretching images horizontally. ('!important'
+  // is disallowed by AMP, so we can't use it to override the width.)
+  max-width: 100%;
 }

render/std_inline.go
@@ -1,11 +1,11 @@
-// Code generated by gen_filemap.go from f5036a4585956c96b8e846b1d4e519b2e8cd77790ed29dd64a1162798a8e8aaf. DO NOT EDIT.
+// Code generated by gen_filemap.go from 528204c42aa8ff2490832e169849ef3c102777eede15a84dc2f4295694eb9da0. DO NOT EDIT.
 
 package render
 
 var stdInline = map[string]string{
 	"amp-boilerplate-noscript.css": "body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}",
 	"amp-boilerplate.css":          "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}",
-	"amp.css":                      "amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{width:auto !important}\n",
+	"amp.css":                      "amp-img.thumb{filter:blur(12px)}main .box>.body .mapbox amp-img[placeholder]{max-width:100%}\n",
 	"base-body.js":                 "applyTheme(); // defined in dark.js\n",
 	"base.css":                     "header .dark{cursor:pointer}main .box{display:block}main .box>.body:after{clear:both;content:'';display:block}main .box>.body>*:first-child,main .box>.body>*:first-child>h3:first-child{margin-top:0}main .box>.body>*:last-child{margin-bottom:0}main .box>.body figure.left{float:left}main .box>.body figure.right{float:right}main .box>.body figure.center{margin-left:auto;margin-right:auto}main .box>.body figure *{max-width:100%}main .box>.body figure img{border:0;display:block;height:auto}main .box>.body img.inline,main .box>.body amp-img.inline{vertical-align:middle}main .box>.body img.pixelated,main .box>.body amp-img.pixelated{image-rendering:pixelated}main .box>.body img.inline{display:inline}main .box>.body pre{max-width:100%;white-space:pre-wrap;word-wrap:break-word}main .box>.body table{border-collapse:collapse}main .box>.body .clear{clear:both}main .box>.body .small{font-size:90%}main .box>.body .real-small{font-size:80%}main .box>.body .no-select{user-select:none}\n",
 	"base.js":                      "document.addEventListener('DOMContentLoaded', () => {\n  // Toggle the navbox when the logo or anything in its title are clicked.\n  const toggle = () => {\n    document.querySelector('nav ul').classList.toggle('collapsed-mobile');\n    document.querySelector('nav .toggle').classList.toggle('expand');\n  };\n  document.querySelector('header .logo').addEventListener('click', toggle);\n  document.querySelector('nav .box .title').addEventListener('click', toggle);\n\n  // |darkQuery| and applyTheme() are defined in dark.js.\n  // Toggle the theme when the dark-mode icon is clicked.\n  // The initial state is set in base-body.js: we can't do this in the top level\n  // of this file since document.body isn't available, and we also don't want to\n  // do it in DOMContentLoaded since we'll get a flash of the light theme then.\n  document\n    .querySelector('header .dark')\n    .addEventListener('click', () => applyTheme(true));\n\n  // We may also need to update the theme if prefers-color-scheme changes.\n  darkQuery.addEventListener('change', () => applyTheme());\n});\n",