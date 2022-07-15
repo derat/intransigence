derat/intransigence

commit 3a00adf7c18275cf1e3bca6281d2c1890a81fe3a

build: Set timestamps independently of old output dir.
Set generated HTML files' mtimes and atimes based on those
of the source Markdown and YAML files, and preserve
timestamps when copying static data.

build/gen.go
@@ -31,7 +31,8 @@ const (
 // generatePages renders non-AMP and AMP versions of all normal pages and writes them
 // to the appropriate subdirectory under out. The generated files' paths are returned.
 // The returned feed info structs are sorted newest-to-oldest.
-func generatePages(si *render.SiteInfo, out string, pretty bool) ([]string, []render.PageFeedInfo, error) {
+func generatePages(si *render.SiteInfo, out string, pretty bool,
+	exeTime time.Time) ([]string, []render.PageFeedInfo, error) {
 	ps, err := filepath.Glob(filepath.Join(si.PageDir(), "*.md"))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to enumerate pages: %v", err)
@@ -43,6 +44,10 @@ func generatePages(si *render.SiteInfo, out string, pretty bool) ([]string, []re
 	for i, p := range ps {
 		statusf("Generating pages: [%d/%d]", i, len(ps))
 		md, err := ioutil.ReadFile(p)
+		if err != nil {
+			return nil, nil, err
+		}
+		pi, err := os.Stat(p)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -64,7 +69,11 @@ func generatePages(si *render.SiteInfo, out string, pretty bool) ([]string, []re
 					return fmt.Errorf("failed to pretty-print %s: %v", filepath.Base(dest), err)
 				}
 			}
-			return ioutil.WriteFile(dest, b, fileMode)
+			if err := ioutil.WriteFile(dest, b, fileMode); err != nil {
+				return err
+			}
+			// Copy the Markdown file's mtime and atime.
+			return os.Chtimes(dest, maxTime(getAtime(pi), exeTime), maxTime(pi.ModTime(), exeTime))
 		}
 
 		if err := build(filepath.Join(out, base+render.HTMLExt), false /* amp */); err != nil {
@@ -84,7 +93,8 @@ func generatePages(si *render.SiteInfo, out string, pretty bool) ([]string, []re
 
 // generateIframes renders all iframe pages and writes them to the appropriate subdirectory under out.
 // The generated files' paths are returned.
-func generateIframes(si *render.SiteInfo, out string, pretty bool) ([]string, error) {
+func generateIframes(si *render.SiteInfo, out string, pretty bool,
+	exeTime time.Time) ([]string, error) {
 	ps, err := filepath.Glob(filepath.Join(si.IframeDir(), "*.yaml"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to enumerate iframe data: %v", err)
@@ -95,6 +105,10 @@ func generateIframes(si *render.SiteInfo, out string, pretty bool) ([]string, er
 	var outPaths []string
 	for _, p := range ps {
 		data, err := ioutil.ReadFile(p)
+		if err != nil {
+			return nil, err
+		}
+		fi, err := os.Stat(p)
 		if err != nil {
 			return nil, err
 		}
@@ -116,6 +130,8 @@ func generateIframes(si *render.SiteInfo, out string, pretty bool) ([]string, er
 		if err := ioutil.WriteFile(dest, b, fileMode); err != nil {
 			return nil, err
 		}
+		// Copy the data file's mtime and atime.
+		return nil, os.Chtimes(dest, maxTime(getAtime(fi), exeTime), maxTime(fi.ModTime(), exeTime))
 	}
 	return outPaths, nil
 }
@@ -152,6 +168,9 @@ func generateCSS(src, dst string) error {
 		dp := filepath.Join(dst, base[:len(base)-4]+"css")
 		if err := exec.Command("sassc", "--style", "compressed", p, dp).Run(); err != nil {
 			return fmt.Errorf("failed running sassc on %v: %v", p, err)
+		}
+		if err := copyTimes(p, dp); err != nil {
+			return err
 		}
 	}
 	return nil
@@ -224,6 +243,9 @@ func generateWebP(src, dst string) error {
 			if err := exec.Command("cwebp", "-preset", preset, ip, "-o", wp).Run(); err != nil {
 				return fmt.Errorf("failed running cwebp on %v: %v", ip, err)
 			}
+		}
+		if err := copyTimes(ip, wp); err != nil {
+			return err
 		}
 		num++
 	}

build/build_test.go
@@ -223,11 +223,18 @@ func TestBuild_Full(t *testing.T) {
 		`<li><span\s+class="selected">Cheshire</span>`, // omit_from_menu
 	})
 
-	// Static data should be copied into the output directory.
-	compareFiles(t, filepath.Join(out, "static.html"), filepath.Join(dir, "static/static.html"), contentsEqual)
-	compareFiles(t, filepath.Join(out, "favicon.ico"), filepath.Join(dir, "static/favicon.ico"), contentsEqual)
-	compareFiles(t, filepath.Join(out, "scottish_fold/nyan.gif"), filepath.Join(dir, "static/scottish_fold/nyan.gif"), contentsEqual)
-	compareFiles(t, filepath.Join(out, "other/extra.html"), filepath.Join(dir, "extra/extra.html"), contentsEqual)
+	// Page mtimes should match that of the original Markdown files.
+	compareFiles(t, filepath.Join(out, "index.html"), filepath.Join(dir, "pages/index.md"), mtimeEqual)
+	compareFiles(t, filepath.Join(out, "index.amp.html"), filepath.Join(dir, "pages/index.md"), mtimeEqual)
+	compareFiles(t, filepath.Join(out, "scottish_fold.html"), filepath.Join(dir, "pages/scottish_fold.md"), mtimeEqual)
+	compareFiles(t, filepath.Join(out, "scottish_fold.amp.html"), filepath.Join(dir, "pages/scottish_fold.md"), mtimeEqual)
+
+	// Static data should be copied into the output directory with mtimes preserved.
+	compareFiles(t, filepath.Join(out, "static.html"), filepath.Join(dir, "static/static.html"), contentsEqual|mtimeEqual)
+	compareFiles(t, filepath.Join(out, "favicon.ico"), filepath.Join(dir, "static/favicon.ico"), contentsEqual|mtimeEqual)
+	compareFiles(t, filepath.Join(out, "scottish_fold/nyan.gif"), filepath.Join(dir, "static/scottish_fold/nyan.gif"),
+		contentsEqual|mtimeEqual)
+	compareFiles(t, filepath.Join(out, "other/extra.html"), filepath.Join(dir, "extra/extra.html"), contentsEqual|mtimeEqual)
 
 	// Textual files should be gzipped, and the .gz file should have the same mtime as the original file.
 	compareFiles(t, filepath.Join(out, "index.html.gz"), filepath.Join(out, "index.html"), contentsEqual|mtimeEqual)
@@ -341,21 +348,24 @@ func TestBuild_Rebuild(t *testing.T) {
 	out := filepath.Join(dir, outSubdir)
 	oldOut := filepath.Join(dir, oldOutSubdir)
 
-	// Unchanged files' and directories' mtimes should be copied over from the first build.
-	compareFiles(t, out, oldOut, mtimeEqual)
+	// Unchanged files' and directories' mtimes should still be the same.
 	compareFiles(t, filepath.Join(out, "cats.html"), filepath.Join(oldOut, "cats.html"), contentsEqual|mtimeEqual)
 	compareFiles(t, filepath.Join(out, "cats.html.gz"), filepath.Join(oldOut, "cats.html.gz"), contentsEqual|mtimeEqual)
 	compareFiles(t, filepath.Join(out, "scottish_fold"), filepath.Join(oldOut, "scottish_fold"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "scottish_fold/maru-400.jpg"),
 		filepath.Join(oldOut, "scottish_fold/maru-400.jpg"), contentsEqual|mtimeEqual)
 	compareFiles(t, filepath.Join(out, "static.html"), filepath.Join(oldOut, "static.html"), contentsEqual|mtimeEqual)
+	compareFiles(t, filepath.Join(out, "other"), filepath.Join(oldOut, "other"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "other/extra.html"), filepath.Join(oldOut, "other/extra.html"), contentsEqual|mtimeEqual)
-	compareFiles(t, out, oldOut, mtimeEqual)
 
 	// The new index file should have the newly-added content and an updated mtime.
 	checkPageContents(t, filepath.Join(out, "index.html"), []string{regexp.QuoteMeta(newContent)}, nil)
 	compareFiles(t, filepath.Join(out, "index.html"), filepath.Join(oldOut, "index.html"), mtimeAfter)
+	compareFiles(t, filepath.Join(out, "index.html"), filepath.Join(dir, "pages/index.md"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "index.html"), filepath.Join(out, "index.html.gz"), contentsEqual|mtimeEqual)
+
+	// The new output directory's mtime should be after the old dir's mtime.
+	compareFiles(t, out, oldOut, mtimeAfter)
 
 	if t.Failed() {
 		fmt.Println("Output is in", out)
@@ -374,8 +384,8 @@ func newTestSiteDir() (string, error) {
 	if err := copy.Copy(src, dir, copy.Options{
 		// Make sure that we don't pick up any random output that happened to be
 		// in the example directory.
-		Skip: func(p string) bool {
-			return strings.HasPrefix(p, src+"/out") || strings.HasPrefix(p, src+"/.out.")
+		Skip: func(p string) (bool, error) {
+			return strings.HasPrefix(p, src+"/out") || strings.HasPrefix(p, src+"/.out."), nil
 		},
 	}); err != nil {
 		os.RemoveAll(dir)

build/build.go
@@ -11,17 +11,19 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/derat/intransigence/render"
 	"github.com/otiai10/copy"
 )
 
 const (
-	siteFile     = "site.yaml"   // YAML file containing render.SiteInfo data
-	outSubdir    = "out"         // subdir under site dir for generated site
-	tmpOutPrefix = ".out."       // prefix passed to ioutil.TempDir for temp output dir
-	oldOutSubdir = ".out.old"    // subdir under site dir containing previous generated site
-	sitemapFile  = "sitemap.xml" // generated XML file containing sitemap data
+	siteFile     = "site.yaml"     // YAML file containing render.SiteInfo data
+	outSubdir    = "out"           // subdir under site dir for generated site
+	tmpOutPrefix = ".out."         // prefix passed to ioutil.TempDir for temp output dir
+	oldOutSubdir = ".out.old"      // subdir under site dir containing previous generated site
+	sitemapFile  = "sitemap.xml"   // generated XML file containing sitemap data
+	exeName      = "intransigence" // executable filename
 )
 
 // Flags specifies details of how the site should be built.
@@ -72,12 +74,13 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 		return err
 	}
 
+	exeTime := getExeTime()
 	var genPaths []string
 	var feedInfos []render.PageFeedInfo
-	if genPaths, feedInfos, err = generatePages(si, out, flags&PrettyPrint != 0); err != nil {
+	if genPaths, feedInfos, err = generatePages(si, out, flags&PrettyPrint != 0, exeTime); err != nil {
 		return err
 	}
-	if ps, err := generateIframes(si, out, flags&PrettyPrint != 0); err != nil {
+	if ps, err := generateIframes(si, out, flags&PrettyPrint != 0, exeTime); err != nil {
 		return err
 	} else {
 		genPaths = append(genPaths, ps...)
@@ -91,7 +94,8 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 	}
 
 	// Copy over static content.
-	if err := copy.Copy(si.StaticDir(), out); err != nil {
+	copyOpts := copy.Options{PreserveTimes: true}
+	if err := copy.Copy(si.StaticDir(), out, copyOpts); err != nil {
 		return err
 	}
 	for src, dst := range si.ExtraStaticDirs {
@@ -99,11 +103,10 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 		if !strings.HasPrefix(dp, out+"/") {
 			return fmt.Errorf("dest path %v escapes out dir", dp)
 		}
-		if err := copy.Copy(filepath.Join(dir, src), dp); err != nil {
+		if err := copy.Copy(filepath.Join(dir, src), dp, copyOpts); err != nil {
 			return err
 		}
 	}
-
 	// Also copy over generated WebP images.
 	gd := si.StaticGenDir()
 	if err := filepath.Walk(gd, func(p string, fi os.FileInfo, err error) error {
@@ -111,13 +114,14 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 			return err
 		}
 		if fi.Mode()&os.ModeType == 0 && filepath.Ext(p) == render.WebPExt {
-			return copy.Copy(p, filepath.Join(out, p[len(gd)+1:]))
+			return copy.Copy(p, filepath.Join(out, p[len(gd)+1:]), copyOpts)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
 
+	// TODO: Try to preserve sitemap and feed timestamps somehow?
 	if err := writeSitemap(filepath.Join(out, sitemapFile), si); err != nil {
 		return fmt.Errorf("sitemap failed: %v", err)
 	}
@@ -128,6 +132,11 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 	// Create gzipped versions of text-based files for the HTTP server to use.
 	if err := compressDir(out); err != nil {
 		return err
+	}
+
+	// Update directory timestamps to improve rsync performance.
+	if _, _, err := updateDirTimes(out); err != nil {
+		return fmt.Errorf("updating dir times failed: %v", err)
 	}
 
 	// If we built into the site dir, rename the temp dir that we used.
@@ -143,10 +152,6 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 				} else if !ok {
 					return errors.New("diff rejected")
 				}
-			}
-			// Copy the existing output dir's atimes and mtimes for dirs and files that didn't change.
-			if err := copyTimes(dest, out); err != nil {
-				return err
 			}
 			// Delete the old backup if present and back up the existing output dir.
 			old := filepath.Join(dir, oldOutSubdir)
@@ -164,4 +169,18 @@ func Build(ctx context.Context, dir, out string, flags Flags) error {
 		}
 	}
 	return nil
+}
+
+// getExeTime returns the intransigence executable's mtime.
+// The zero time is returned during testing or if an error is encountered.
+func getExeTime() time.Time {
+	exe, err := os.Executable()
+	if err != nil || filepath.Base(exe) != exeName {
+		return time.Time{}
+	}
+	fi, err := os.Stat(exe)
+	if err != nil {
+		return time.Time{}
+	}
+	return fi.ModTime()
 }

build/compress.go
@@ -1,0 +1,56 @@
+// Copyright 2022 Daniel Erat.
+// All rights reserved.
+
+package build
+
+import (
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// compressDir writes compressed versions of textual files within dir (including in subdirs).
+func compressDir(dir string) error {
+	return filepath.Walk(dir, func(p string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.Mode()&os.ModeType != 0 {
+			return nil
+		}
+		switch filepath.Ext(p) {
+		case ".css", ".htm", ".html", ".js", ".json", ".txt", ".xml":
+			dp := p + ".gz"
+			if err := compressFile(p, dp); err != nil {
+				return err
+			}
+			// Give the .gz file the same mtime and atime as the original file so rsync can skip it.
+			return os.Chtimes(dp, getAtime(fi), fi.ModTime())
+		default:
+			return nil
+		}
+	})
+}
+
+// compressFile writes a gzipped version of sp at dp.
+// The modification time in the gzip header is unset.
+func compressFile(sp, dp string) error {
+	sf, err := os.Open(sp)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+
+	df, err := os.Create(dp)
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+
+	w := gzip.NewWriter(df)
+	if _, err := io.Copy(w, sf); err != nil {
+		return err
+	}
+	return w.Close()
+}

build/proc.go
@@ -1,167 +1,0 @@
-// Copyright 2020 Daniel Erat <dan@erat.org>.
-// All rights reserved.
-
-package build
-
-import (
-	"bytes"
-	"compress/gzip"
-	"crypto/sha256"
-	"io"
-	"os"
-	"path/filepath"
-	"reflect"
-	"sort"
-	"syscall"
-	"time"
-)
-
-// compressDir writes compressed versions of textual files within dir (including in subdirs).
-func compressDir(dir string) error {
-	return filepath.Walk(dir, func(p string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if fi.Mode()&os.ModeType != 0 {
-			return nil
-		}
-		switch filepath.Ext(p) {
-		case ".css", ".htm", ".html", ".js", ".json", ".txt", ".xml":
-			dp := p + ".gz"
-			if err := compressFile(p, dp); err != nil {
-				return err
-			}
-			// Give the .gz file the same mtime and atime as the original file so rsync can skip it.
-			return os.Chtimes(dp, getAtime(fi), fi.ModTime())
-		default:
-			return nil
-		}
-	})
-}
-
-// compressFile writes a gzipped version of sp at dp.
-// The modification time in the gzip header is unset.
-func compressFile(sp, dp string) error {
-	sf, err := os.Open(sp)
-	if err != nil {
-		return err
-	}
-	defer sf.Close()
-
-	df, err := os.Create(dp)
-	if err != nil {
-		return err
-	}
-	defer df.Close()
-
-	w := gzip.NewWriter(df)
-	if _, err := io.Copy(w, sf); err != nil {
-		return err
-	}
-	return w.Close()
-}
-
-// getAtime returns the atime from fi.
-func getAtime(fi os.FileInfo) time.Time {
-	// See https://stackoverflow.com/a/20877193.
-	stat := fi.Sys().(*syscall.Stat_t)
-	return time.Unix(int64(stat.Atim.Sec), int64(stat.Atim.Nsec))
-}
-
-// copyTimes examines each file in src. If a file with the same contents exists
-// at the same path relative to dst, the source atime and mtime are copied over.
-func copyTimes(src, dst string) error {
-	return filepath.Walk(src, func(sp string, si os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		dp := filepath.Join(dst, sp[len(src):])
-		di, err := os.Stat(dp)
-		if os.IsNotExist(err) {
-			return nil
-		} else if err != nil {
-			return err
-		}
-
-		if si.Mode() != di.Mode() || si.Size() != di.Size() {
-			return nil
-		}
-		switch {
-		case di.Mode()&os.ModeDir != 0: // directory
-			if match, err := dirsMatch(sp, dp); err != nil {
-				return err
-			} else if !match {
-				return nil
-			}
-		case di.Mode()&os.ModeType == 0: // regular file
-			if match, err := filesMatch(sp, dp); err != nil {
-				return err
-			} else if !match {
-				return nil
-			}
-		default:
-			// TODO: Handle symlinks?
-			return nil
-		}
-
-		// If we didn't bail out, update the times to match.
-		return os.Chtimes(dp, getAtime(si), si.ModTime())
-	})
-}
-
-// dirsMatch returns true if the directories at paths a and b have the same contents (i.e. filenames).
-func dirsMatch(a, b string) (bool, error) {
-	getNames := func(p string) ([]string, error) {
-		f, err := os.Open(p)
-		if err != nil {
-			return nil, err
-		}
-		defer f.Close()
-
-		names, err := f.Readdirnames(0)
-		if err != nil {
-			return nil, err
-		}
-		sort.Strings(names)
-		return names, nil
-	}
-
-	an, err := getNames(a)
-	if err != nil {
-		return false, err
-	}
-	bn, err := getNames(b)
-	if err != nil {
-		return false, err
-	}
-	return reflect.DeepEqual(an, bn), nil
-}
-
-// filesMatch reads the files at paths a and b and returns true if they have the same contents.
-func filesMatch(a, b string) (bool, error) {
-	ah, err := hashFile(a)
-	if err != nil {
-		return false, err
-	}
-	bh, err := hashFile(b)
-	if err != nil {
-		return false, err
-	}
-	return bytes.Equal(ah, bh), nil
-}
-
-// hashFile reads the file at p and returns a SHA256 hash of its contents.
-func hashFile(p string) ([]byte, error) {
-	f, err := os.Open(p)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
-		return nil, err
-	}
-	return h.Sum(nil), nil
-}

build/time.go
@@ -1,0 +1,68 @@
+// Copyright 2020 Daniel Erat <dan@erat.org>.
+// All rights reserved.
+
+package build
+
+import (
+	"os"
+	"path/filepath"
+	"syscall"
+	"time"
+)
+
+// getAtime returns the atime from fi.
+func getAtime(fi os.FileInfo) time.Time {
+	// See https://stackoverflow.com/a/20877193.
+	stat := fi.Sys().(*syscall.Stat_t)
+	return time.Unix(int64(stat.Atim.Sec), int64(stat.Atim.Nsec))
+}
+
+// maxTime returns the max of the supplied times.
+func maxTime(t0 time.Time, ts ...time.Time) time.Time {
+	max := t0
+	for _, t := range ts {
+		if t.After(max) {
+			max = t
+		}
+	}
+	return max
+}
+
+// copyTimes copies the first path's atime and mtime to the second path.
+func copyTimes(from, to string) error {
+	fi, err := os.Stat(from)
+	if err != nil {
+		return err
+	}
+	return os.Chtimes(to, getAtime(fi), fi.ModTime())
+}
+
+// updateDirTimes recursively updates dir and its subdirectories
+// to have the max atime and mtime of their contents.
+func updateDirTimes(dir string) (atime, mtime time.Time, err error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return atime, mtime, err
+	}
+	for _, e := range entries {
+		fi, err := e.Info()
+		if err != nil {
+			return atime, mtime, err
+		}
+		var at, mt time.Time
+		if fi.IsDir() {
+			if at, mt, err = updateDirTimes(filepath.Join(dir, fi.Name())); err != nil {
+				return atime, mtime, err
+			}
+		} else {
+			at, mt = getAtime(fi), fi.ModTime()
+		}
+		atime = maxTime(atime, at)
+		mtime = maxTime(mtime, mt)
+	}
+	if !atime.IsZero() && !mtime.IsZero() {
+		err = os.Chtimes(dir, atime, mtime)
+	}
+	return atime, mtime, err
+
+}

go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/derat/validate v0.0.0-20220207210801-9c9d0eb6729e
 	github.com/gorilla/feeds v1.1.1
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/otiai10/copy v1.1.1
+	github.com/otiai10/copy v1.7.0
 	github.com/pmezard/go-difflib v1.0.0
 	github.com/russross/blackfriday/v2 v2.0.1
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect

go.sum
@@ -24,14 +24,14 @@ github.com/kr/text v0.1.0/go.mod h1:4Jbv+DJW3UT/LiOwJeYQe1efqtUx/iVham/4vfdArNI=
 github.com/kr/text v0.2.0 h1:5Nx0Ya0ZqY2ygV366QzturHI13Jq95ApcVaJBhpS+AY=
 github.com/kr/text v0.2.0/go.mod h1:eLer722TekiGuMkidMxC/pM04lWEeraHUUmBw8l2grE=
 github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2/go.mod h1:0KeJpeMD6o+O4hW7qJOT7vyQPKrWmj26uf5wMc/IiIs=
-github.com/otiai10/copy v1.1.1 h1:PH7IFlRQ6Fv9vYmuXbDRLdgTHoP1w483kPNUP2bskpo=
-github.com/otiai10/copy v1.1.1/go.mod h1:rrF5dJ5F0t/EWSYODDu4j9/vEeYHMkc8jt0zJChqQWw=
+github.com/otiai10/copy v1.7.0 h1:hVoPiN+t+7d2nzzwMiDHPSOogsWAStewq3TwU05+clE=
+github.com/otiai10/copy v1.7.0/go.mod h1:rmRl6QPdJj6EiUqXQ/4Nn2lLXoNQjFCQbbNrxgc/t3U=
 github.com/otiai10/curr v0.0.0-20150429015615-9b4961190c95/go.mod h1:9qAhocn7zKJG+0mI8eUu6xqkFDYS2kb2saOteoSB3cE=
 github.com/otiai10/curr v1.0.0 h1:TJIWdbX0B+kpNagQrjgq8bCMrbhiuX73M2XwgtDMoOI=
 github.com/otiai10/curr v1.0.0/go.mod h1:LskTG5wDwr8Rs+nNQ+1LlxRjAtTZZjtJW4rMXl6j4vs=
 github.com/otiai10/mint v1.3.0/go.mod h1:F5AjcsTsWUqX+Na9fpHb52P8pcRX2CI6A3ctIT91xUo=
-github.com/otiai10/mint v1.3.1 h1:BCmzIS3n71sGfHB5NMNDB3lHYPz8fWSkCAErHed//qc=
-github.com/otiai10/mint v1.3.1/go.mod h1:/yxELlJQ0ufhjUwhshSj+wFjZ78CnZ48/1wtmBH1OTc=
+github.com/otiai10/mint v1.3.3 h1:7JgpsBaN0uMkyju4tbYHu0mnM55hNKVYLsXmwr15NQI=
+github.com/otiai10/mint v1.3.3/go.mod h1:/yxELlJQ0ufhjUwhshSj+wFjZ78CnZ48/1wtmBH1OTc=
 github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
 github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
 github.com/rogpeppe/go-internal v1.6.1 h1:/FiVV8dS/e+YqF2JvO3yXRFbBLTIuSDkuC7aBOAvL+k=

commit fb5dc85b82682e76417971777a602c3fc3b06a5a

build: Fix bug where only first iframe was generated.
This was a regression from 3a00adf7. Also make the example
site include a graph and map and add some basic iframe tests
that I should've added earlier.

build/gen.go
@@ -131,7 +131,9 @@ func generateIframes(si *render.SiteInfo, out string, pretty bool,
 			return nil, err
 		}
 		// Copy the data file's mtime and atime.
-		return nil, os.Chtimes(dest, maxTime(getAtime(fi), exeTime), maxTime(fi.ModTime(), exeTime))
+		if err := os.Chtimes(dest, maxTime(getAtime(fi), exeTime), maxTime(fi.ModTime(), exeTime)); err != nil {
+			return nil, err
+		}
 	}
 	return outPaths, nil
 }

example/iframes/graph.yaml
@@ -1,0 +1,15 @@
+---
+graphs:
+  line:
+    title: Here's a graph with a line
+    points:
+      - { time: 1311509160, value: 74 }
+      - { time: 1311511260, value: 108 }
+      - { time: 1311513240, value: 140 }
+      - { time: 1311515100, value: 108 }
+      - { time: 1311516960, value: 111 }
+      - { time: 1311518100, value: 121 }
+    notes:
+      - { time: 1311509220, text: 'Something happened here' }
+    range: [50, 150]
+    units: Units

example/iframes/map.yaml
@@ -1,0 +1,7 @@
+---
+map_placeholder_light: '../scottish_fold/map_light.png'
+map_placeholder_dark: '../scottish_fold/map_dark.png'
+map_points:
+  - name: Somewhere
+    lat_long: [40.306632, -90.817727]
+    id: somewhere

example/inline/graph.scss
@@ -1,0 +1,3 @@
+main .box > .body .graph {
+  border: none;
+}

example/inline/map.scss
@@ -1,0 +1,7 @@
+main .box > .body .mapbox {
+  margin: 4px 0 0 0;
+  // Percentage-based padding is based on the containing block's width:
+  // https://css-tricks.com/oh-hey-padding-percentage-is-based-on-the-parent-elements-width/
+  padding-bottom: 75%; // 75% of width for 4:3 aspect ratio
+  width: 100%;
+}

build/build_test.go
@@ -156,6 +156,8 @@ func TestBuild_Full(t *testing.T) {
 		`<span class="real-small">makes\s+it\s+even\s+smaller</span>`, // <text-size tiny>
 		`Text can also be <span class="no-select">marked as ` + // ‹...›
 			`non-selectable</span> within a code block`,
+		`<iframe[^>]+src="iframes/map\.html"`,         // map iframe
+		`<iframe[^>]+src="iframes/graph\.html\?line"`, // graph iframe
 		`<a href="#top">Back\s+to\s+top</a>`,
 		`Page created in\s+<time datetime="2020">2020</time>\.`,
 		`Last modified\s+<time datetime="2020-05-21">May 21, 2020</time>\.`,
@@ -223,11 +225,17 @@ func TestBuild_Full(t *testing.T) {
 		`<li><span\s+class="selected">Cheshire</span>`, // omit_from_menu
 	})
 
-	// Page mtimes should match that of the original Markdown files.
+	// Check that iframe HTML files are generated.
+	checkPageContents(t, filepath.Join(out, "iframes/graph.html"), []string{`<a\s+id="graph-node">\s*</a>`}, []string{})
+	checkPageContents(t, filepath.Join(out, "iframes/map.html"), []string{`<div\s+id="map-div">\s*</div>`}, []string{})
+
+	// Page/iframe mtimes should match those of the original Markdown/YAML files.
 	compareFiles(t, filepath.Join(out, "index.html"), filepath.Join(dir, "pages/index.md"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "index.amp.html"), filepath.Join(dir, "pages/index.md"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "scottish_fold.html"), filepath.Join(dir, "pages/scottish_fold.md"), mtimeEqual)
 	compareFiles(t, filepath.Join(out, "scottish_fold.amp.html"), filepath.Join(dir, "pages/scottish_fold.md"), mtimeEqual)
+	compareFiles(t, filepath.Join(out, "iframes/graph.html"), filepath.Join(dir, "iframes/graph.yaml"), mtimeEqual)
+	compareFiles(t, filepath.Join(out, "iframes/map.html"), filepath.Join(dir, "iframes/map.yaml"), mtimeEqual)
 
 	// Static data should be copied into the output directory with mtimes preserved.
 	compareFiles(t, filepath.Join(out, "static.html"), filepath.Join(dir, "static/static.html"), contentsEqual|mtimeEqual)