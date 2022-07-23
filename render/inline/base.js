a07210f1d524b232d32eccac5dbde317213d488e
Daniel Erat <dan@erat.org>
2022-07-23 09:25:22 -0400

    render: Improve navbox expand/collapse animations.
    Set the navbox's start and end height in JavaScript to make
    animations feel more responsive.

--- render/inline/base.js
+++ render/inline/base.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.sitenav');
+  const navBody = nav.querySelector('.box > .body');
+  const navList = navBody.querySelector('ul');
+  const navPadding = 32; // >= navBody's non-collapsed padding
+
   // Toggle the navbox when the logo or anything in its title are clicked.
-  const toggle = () => {
-    document.querySelector('.sitenav').classList.toggle('collapsed-mobile');
-    document.querySelector('.sitenav .toggle').classList.toggle('expand');
+  const toggleNav = () => {
+    // Animating height is a mess: https://stackoverflow.com/questions/3508605
+    // When collapsing, set max-height to the actual height first so the
+    // animation begins immediately. When expanding, set it to list's height
+    // (plus extra for padding) so the animation takes roughly the right time.
+    if (!nav.classList.contains('collapsed-mobile')) {
+      navBody.style.maxHeight = navBody.clientHeight + 'px';
+      window.setTimeout(() => (navBody.style.maxHeight = ''));
+    } else {
+      navBody.style.maxHeight = navList.clientHeight + navPadding + 'px';
+    }
+    nav.classList.toggle('collapsed-mobile');
   };
-  document.querySelector('header .logo').addEventListener('click', toggle);
+  document.querySelector('header .logo').addEventListener('click', toggleNav);
   document
     .querySelector('.sitenav .box .title')
-    .addEventListener('click', toggle);
+    .addEventListener('click', toggleNav);
+
+  // At the end of a transition, tell the body to use its natural height in case
+  // the window is later resized.
+  navBody.addEventListener('transitionend', () => {
+    navBody.style.maxHeight = '';
+  });
 
   // |darkQuery| and applyTheme() are defined in dark.js.
   // Toggle the theme when the dark-mode icon is clicked.
